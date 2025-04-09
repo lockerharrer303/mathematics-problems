315b301655dd10ca903f49c1828eba7612a2544f
lockerharrer303 <lockerharrer303@gmail.com>
2025-04-09 20:58:20 +0000

    Add 31.py

---
+++ src/31.py
@@ -0,0 +1,15 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+num = int(input("Enter a number: "))
+if is_prime(num):
+    print(f"{num} is a prime number.")
+else:
+    print(f"{num} is not a prime number.")
+
+# Additional exercise code (you can choose to write additional exercises in the next line)
