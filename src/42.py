b74b5e8d83a89ad985625366b7bca4a02e62c996
lockerharrer303 <lockerharrer303@gmail.com>
2025-04-09 21:11:08 +0000

    Add 42.py

---
+++ src/42.py
@@ -0,0 +1,7 @@
+import math
+pi = 3.141592653589793
+r = float(input("Enter the radius of the circle: "))
+area = pi * r**2
+circumference = 2 * pi * r
+print(f"The area of the circle with radius {r} is {area:.2f}")
+print(f"The circumference of the circle with radius {r} is {circumference:.2f}")
