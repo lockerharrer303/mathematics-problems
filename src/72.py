789a04ed41decb96b09fba02adf036573702b028
lockerharrer303 <lockerharrer303@gmail.com>
2025-04-11 12:26:24 +0000

    Add 72.py

---
+++ src/72.py
@@ -0,0 +1,8 @@
+def calculate_rectangle_area(length, width):
+    area = length * width
+    return area
+
+length = 5
+width = 3
+area = calculate_rectangle_area(length, width)
+print(f"The area of the rectangle is: {area}")
